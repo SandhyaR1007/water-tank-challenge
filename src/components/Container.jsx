a4645e68556a5c5b55c455a3d3c263c5f624a1b0
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2025-01-26 17:23:01 +0530

    update: login for equating water levels

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Tank from "./Tank";
 
 const Container = () => {
+  const [tanks, setTanks] = useState([0, 0, 0, 0]);
+  const handleEquate = () => {
+    const portion = Math.floor(Math.max(...tanks) / 4);
+
+    setTanks((prevTanks) =>
+      prevTanks.map((w, i) =>
+        w !== portion ? w + handleWaterLevel(w, portion) : w
+      )
+    );
+    function handleWaterLevel(curr, req) {
+      if (curr > req) {
+        if (curr - req < 25) {
+          return -(curr - req);
+        } else {
+          return -25;
+        }
+      } else {
+        if (req - curr < 25) {
+          return req - curr;
+        } else {
+          return 25;
+        }
+      }
+    }
+  };
   return (
     <div className="container">
-      <Tank />
-      <Tank />
-      <Tank />
-      <Tank />
+      {tanks.map((w, i) => (
+        <Tank
+          key={i}
+          setTanks={setTanks}
+          tanks={tanks}
+          index={i}
+          handleEquate={handleEquate}
+        />
+      ))}
     </div>
   );
 };
